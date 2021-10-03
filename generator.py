db2a9ac223e581aaf5a7e30c0c56aa4a6638451f
Mathys L <lopintomathys@gmail.com>
2021-10-03 18:44:44 +0200

    New encoding system
    An better encoding system

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+import base64
 """
 The program generator. You will find here everything that you need to complete
 yourself for set up the program.
@@ -6,10 +7,11 @@ yourself for set up the program.
 
 # modes are the name of the different modes you want to be existing
 modes = (
-    "Classic",                 # mode 0
-    "with special characters"  # mode 1
+    "Classic",                  # mode 0
+    "with no special characters",  # mode 1
+    "with no digit",            # mode 2
+    "text only",                # mode 3
 )
-
 
 def generate(key: str, mode=0) -> str:
     """
@@ -19,9 +21,32 @@ def generate(key: str, mode=0) -> str:
     :param mode: The generation mode
     :return: the generated password
     """
+ # You can edit "random" with anything
 
     if mode == 0:
-        return "1965" + key             # generator for mode 0
-
+        key = "random" + key
+        message_bytes = key.encode('UTF-8')
+        base64_bytes = base64.b64encode(message_bytes)
+        base64_message = base64_bytes.decode('UTF-8')
+        chiffre = base64_message + str(len(base64_message))
+        end = chiffre + "*"
+        return end
     elif mode == 1:
-        return "1965" + key + "@"       # generator for mode 1
+        key = "random" + key
+        message_bytes = key.encode('UTF-8')
+        base64_bytes = base64.b64encode(message_bytes)
+        base64_message = base64_bytes.decode('UTF-8')
+        end = base64_message + str(len(base64_message))
+        return end
+    elif mode == 2:
+        key = "random" + key
+        message_bytes = key.encode('UTF-8')
+        base64_bytes = base64.b64encode(message_bytes)
+        base64_message = base64_bytes.decode('UTF-8')
+        end = base64_message + "*"
+        return end
+    elif mode == 3:
+        key = "random" + key
+        message_bytes = key.encode('UTF-8')
+        end = base64.b64encode(message_bytes)
+        return end
